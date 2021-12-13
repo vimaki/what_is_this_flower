97e91208ea3df8a3cc95e768055e3ccefe66a837
Vitaly.Makin <makin.vitaly@gmail.com>
2021-12-13 15:52:52 +0300

    Added docstring in the scraping_dataset/__init__.py

--- scraping_dataset/__init__.py
+++ scraping_dataset/__init__.py
@@ -1,2 +1,18 @@
+"""Automatic collection of dataset using google image search.
+
+Modules
+-------
+image_scraper
+    It collects links to images on a given topic.
+create_dataset
+    Downloads images corresponding to the list of subjects.
+
+References
+----------
+flower_types.json
+    A text file containing all the required types of flowers and their
+    corresponding translations into Russian.
+"""
+
 from .image_scraper import *
 from .create_dataset import *
