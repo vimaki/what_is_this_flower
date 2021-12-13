39b3824860977681da6f9221fcc4eb541e398837
Vitaly.Makin <makin.vitaly@gmail.com>
2021-12-13 13:51:04 +0300

    Added docstrings in the images_visualization.py

--- predictive_model/model_utils/images_visualization.py
+++ predictive_model/model_utils/images_visualization.py
@@ -1,3 +1,21 @@
+"""Visualizations associated with printing images.
+
+Functions
+---------
+imshow_tensor
+    Display Pytorch Tensor as an image.
+show_dataset_examples
+    Display sample images from a dataset.
+show_images_with_predictions
+    Print images with true and predicted classes.
+
+References
+----------
+load_dataset.py
+    A module that overrides the Dataset class called MyDataset that
+    stores images and their labels.
+"""
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
@@ -9,7 +27,7 @@ from .load_dataset import MyDataset
 
 
 def imshow_tensor(inp: Tensor, title: bool = None, plt_ax=plt) -> None:
-    """Analogue of the Imshow function for tensors"""
+    """Display Pytorch Tensor as an image."""
     inp = inp.numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
@@ -22,7 +40,7 @@ def imshow_tensor(inp: Tensor, title: bool = None, plt_ax=plt) -> None:
 
 
 def show_dataset_examples(dataset: MyDataset, n_samples: int = 16) -> None:
-    """Display sample images from a dataset"""
+    """Display sample images from a dataset."""
     n_rows = ceil(n_samples / 4)
     if n_rows == 1:
         n_columns = n_samples
@@ -41,7 +59,31 @@ def show_dataset_examples(dataset: MyDataset, n_samples: int = 16) -> None:
 
 
 def show_images_with_predictions(dataset: MyDataset, predictions: np.ndarray,
-                                 n_samples=9) -> None:
+                                 n_samples: int = 9) -> None:
+    """Print images with true and predicted classes.
+
+    N images randomly selected from the dataset are printed. Above the
+    image, its real class is displayed, and the class predicted by the
+    model is applied to the image, as well as the model's confidence in
+    its prediction, expressed as a percentage.
+
+    Parameters
+    ----------
+    dataset : MyDataset
+        An object that contains images and their labels.
+    predictions : np.ndarray
+        Model predictions for passed data. A numpy array containing an
+        arrays of floats that match the model's confidence in assigning
+        each of the classes to the image.
+    n_samples : int
+        The number of randomly selected images for which classification
+        information will be displayed.
+
+    Returns
+    -------
+    None
+    """
+
     n_rows = ceil(n_samples / 3)
     if n_rows == 1:
         n_columns = n_samples
@@ -60,19 +102,15 @@ def show_images_with_predictions(dataset: MyDataset, predictions: np.ndarray,
 
         imshow_tensor(image.data.cpu(), title=ground_truth, plt_ax=fig_x)
 
-        fig_x.add_patch(patches.Rectangle((0, 53), 86, 35, color='white'))
+        fig_x.add_patch(patches.Rectangle((0, 53), 86, 20, color='white'))
         font0 = FontProperties()
         font = font0.copy()
-        font.set_family('fantasy')
 
-        # prob_pred = predict_one_sample(model_ensemble, im_val.unsqueeze(0))
         image_prediction = predictions[random_character]
         predicted_proba = np.max(image_prediction) * 100
         class_prediction = np.argmax(image_prediction)
 
         predicted_label = dataset.label_encoder.classes_[class_prediction]
-        # predicted_label = (predicted_label[:len(predicted_label) // 2] +
-        #                    '\n' + predicted_label[len(predicted_label) // 2:])
         predicted_text = '{} : {:.0f}%'.format(predicted_label, predicted_proba)
 
         fig_x.text(1, 59, predicted_text, horizontalalignment='left',
