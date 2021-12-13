e95ca87fab282f4a672c776c80425687203d618f
Vitaly.Makin <makin.vitaly@gmail.com>
2021-12-13 15:46:56 +0300

    Added docstrings in the model_utils/__init__.py

--- predictive_model/model_utils/__init__.py
+++ predictive_model/model_utils/__init__.py
@@ -1,3 +1,21 @@
+"""Functions and classes for classifying images using neural networks.
+
+Modules
+-------
+images_visualization
+    Visualizations associated with printing images.
+load_dataset
+    Loading data into the format required for the neural network.
+model_predictions
+    Making predictions for transmitted data using a neural network.
+model_quality
+    Checking the quality of the model.
+model_training
+    Neural network training.
+utils
+    Helper functions for training artificial neural networks.
+"""
+
 from .images_visualization import *
 from .load_dataset import *
 from .model_predictions import *
