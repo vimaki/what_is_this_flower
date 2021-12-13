a469170d118157cd7dac3f3e492ae42bc9569236
Vitaly.Makin <makin.vitaly@gmail.com>
2021-12-13 15:11:54 +0300

    Added docstrings in the utils.py

--- predictive_model/model_utils/utils.py
+++ predictive_model/model_utils/utils.py
@@ -1,3 +1,19 @@
+"""Helper functions for training artificial neural networks.
+
+Functions
+---------
+find_init_lr
+    Iterating over different values of the step of gradient descent.
+plot_lr_loss
+    Plot the values of the loss function versus the gradient descent step.
+
+References
+----------
+load_dataset.py
+    A module that overrides the Dataset class called MyDataset that
+    stores images and their labels.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -8,11 +24,54 @@ from typing import List, Tuple
 from .load_dataset import MyDataset
 
 
-def find_lr(model, train_dataset: MyDataset, loss_func, optimizer,
-            init_value: float = 1e-8, final_value: float = 10.0,
-            batch_size: int = 64, on_gpu: bool = True)\
+def find_init_lr(model, train_dataset: MyDataset, loss_func, optimizer,
+                 init_value: float = 1e-8, final_value: float = 10.0,
+                 batch_size: int = 64, on_gpu: bool = True) \
         -> Tuple[List[float], List[float]]:
-    """Iterating over different values of the step of gradient descent"""
+    """Iterating over different values of the step of gradient descent.
+
+    One epoch is produced on the dataset for training. For each new
+    sample, a new gradient descent value is set, depending on the
+    initial value, the final value and the size of the dataset. All
+    values of the gradient descent step and the corresponding values
+    of the loss function are recorded.
+    The possibility of early stopping of the algorithm in case of a sharp
+    increase in the value of the loss function has been implemented.
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        A model that is a specific architecture of an artificial neural
+        network that runs in the Pytorch framework.
+    train_dataset: MyDataset
+        An object that contains images and their labels for training
+        the model.
+    loss_func : torch.nn.Module
+        A function whose values indicate how well the model predicts.
+    optimizer: torch.optim.Optimizer
+        An algorithm that alters the operation of gradient descent
+        to speed up the convergence to a minimum of the loss function.
+    init_value: float, optional
+        The initial value of the gradient descent step from which the
+        search will start (default is 1e-8).
+    final_value: float, optional
+        The final value of the gradient descent step up to which the
+        search will be performed (default is 10.0).
+    batch_size : int, optional
+        The size of the portions of images that will be simultaneously
+        processed by the model (default if 64 samples).
+    on_gpu : bool, optional
+        Flag that determines whether calculations will be performed
+        on a GPU or on a CPU (default is True, i.e. on a GPU).
+
+    Returns
+    -------
+    tuple(list(float), list(float))
+        A two-element tuple containing a list of decimal logarithms
+        of all applied gradient descent step values and a list of
+        corresponding loss function values.
+    """
+
     if on_gpu:
         device = torch.device('cuda')
     else:
@@ -40,7 +99,6 @@ def find_lr(model, train_dataset: MyDataset, loss_func, optimizer,
         # Crash out if loss explodes
         if batch_num > 1 and loss > 4 * best_loss:
             return log_lrs, losses
-            # return log_lrs[10:-5], losses[10:-5]
 
         # Record the best loss
         if loss < best_loss or batch_num == 1:
@@ -61,7 +119,7 @@ def find_lr(model, train_dataset: MyDataset, loss_func, optimizer,
 
 
 def plot_lr_loss(logs_lr: List[float], losses: List[float]) -> None:
-    """Plot the changes of loss function versus the steps of gradient descent"""
+    """Plot the values of the loss function versus the gradient descent step."""
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
     ax.plot(logs_lr, losses)
