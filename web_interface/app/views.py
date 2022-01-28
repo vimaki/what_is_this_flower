1e7e22054897e1bc64bab961a48e7c328d3711bf
Vitaly.Makin <makin.vitaly@gmail.com>
2022-01-28 15:35:37 +0100

    Added docstrings in the views.py

--- web_interface/app/views.py
+++ web_interface/app/views.py
@@ -1,3 +1,26 @@
+"""Module with routes used in the application.
+
+Functions
+---------
+predict
+    Setting the main content of the application.
+display_image
+    Display an image on the same page when a form is submitted.
+
+References
+----------
+__init__.py
+    A module that contains an object that is WSGI application.
+model_inference.py
+    A module that performing flower classification on the transferred
+    image.
+forms.py
+    A module that contains forms used in the application.
+utils.py
+    A module that contains auxiliary functions for implementing the
+    business logic of the application.
+"""
+
 import logging
 import os
 
@@ -15,6 +38,7 @@ logging.basicConfig(level=logging.INFO)
 
 @app.route('/', methods=['GET', 'POST'])
 def predict():
+    """Setting the main content of the application."""
     form = UploadForm()
     if form.validate_on_submit():
         image = form.upload_image.data
@@ -35,4 +59,5 @@ def predict():
 
 @app.route('/display_image/<string:filename>')
 def display_image(filename):
+    """Display an image on the same page when a form is submitted."""
     return redirect(url_for('static', filename='img/' + filename), code=301)
