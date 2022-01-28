d594cbd5b1b6efc48408bae71f3ad4eb5700dd14
Vitaly.Makin <makin.vitaly@gmail.com>
2022-01-28 19:15:53 +0100

    Added docstrings in the run.py

--- web_interface/run.py
+++ web_interface/run.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+
+"""Web application startup file.
+
+References
+----------
+app
+    A package that contains all the components of a web application,
+    in the file __init__ of which an instance of the WSGI application
+    is created.
+"""
+
 from app import app
 
 if __name__ == '__main__':
